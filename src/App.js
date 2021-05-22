5d2f65c7011c6875461b1db0026482c7b595a1ac
yessiborghi <yessicaborghi@gmail.com.ar>
2021-05-21 21:04:59 -0300

    add menu

--- src/App.js
+++ src/App.js
@@ -24,9 +24,7 @@
 import React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
-
-import BlocklyComponent, { Block, Value, Field, Shadow } from './Blockly';
+import BlocklyComponent, { Block, Value, Field, Category } from './Blockly';
 
 import BlocklyJS from 'blockly/javascript';
 
@@ -50,8 +48,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <button onClick={this.generateCode}>Convert</button>
+          <button onClick={this.generateCode} className="button">Convert</button>
           <BlocklyComponent ref={this.simpleWorkspace}
           readOnly={false} trashcan={true} media={'media/'}
           move={{
@@ -60,35 +57,120 @@ class App extends React.Component {
             wheel: true
           }}
           initialXml={`
-<xml xmlns="http://www.w3.org/1999/xhtml">
-<block type="controls_ifelse" x="0" y="0"></block>
-</xml>
-      `}>
-            <Block type="test_react_field" />
-            <Block type="test_campo_field" />
-            <Block type="test_react_date_field" />
-            <Block type="controls_ifelse" />
-            <Block type="logic_compare" />
-            <Block type="logic_operation" />
-            <Block type="controls_repeat_ext">
-              <Value name="TIMES">
-                <Shadow type="math_number">
-                  <Field name="NUM">10</Field>
-                </Shadow>
-              </Value>
-            </Block>
-            <Block type="logic_operation" />
-            <Block type="logic_negate" />
-            <Block type="logic_boolean" />
-            <Block type="logic_null" disabled="true" />
-            <Block type="logic_ternary" />
-            <Block type="text_charAt">
-              <Value name="VALUE">
-                <Block type="variables_get">
-                  <Field name="VAR">text</Field>
-                </Block>
-              </Value>
-            </Block>
+            <xml xmlns="http://www.w3.org/1999/xhtml">
+            </xml>
+          `}>
+
+            <Category name="Logic" colour="160">  
+              <Category name="If" colour="%{BKY_LOGIC_HUE}">
+                  <Block type="controls_if"></Block>
+                  <Block type="controls_if">
+                    <mutation else="1"></mutation>
+                  </Block>
+                  <Block type="controls_if">
+                    <mutation elseif="1" else="1"></mutation>
+                  </Block>
+              </Category>
+              <Category name="Boolean" colour="%{BKY_LOGIC_HUE}">
+                  <Block type="logic_compare"></Block>
+                  <Block type="logic_operation"></Block>
+                  <Block type="logic_negate"></Block>
+                  <Block type="logic_boolean"></Block>
+                  <Block type="logic_null"></Block>
+                  <Block type="logic_ternary"></Block>
+                </Category>
+          </Category>
+            <Category name="Loops" colour="%{BKY_LOOPS_HUE}">
+              <Block type="controls_repeat_ext">
+                <Value name="TIMES">
+                  <Block type="math_number">
+                    <Field name="NUM">10</Field>
+                  </Block>
+                </Value>
+              </Block>
+              <Block type="controls_whileUntil"></Block>
+              <Block type="controls_for">
+                <Field name="VAR">i</Field>
+                <Value name="FROM">
+                  <Block type="math_number">
+                    <Field name="NUM">1</Field>
+                  </Block>
+                </Value>
+                <Value name="TO">
+                  <Block type="math_number">
+                    <Field name="NUM">10</Field>
+                  </Block>
+                </Value>
+                <Value name="BY">
+                  <Block type="math_number">
+                    <Field name="NUM">1</Field>
+                  </Block>
+                </Value>
+              </Block>
+              <Block type="controls_forEach"></Block>
+              <Block type="controls_flow_statements"></Block>
+            </Category>
+            <Category name="Math" colour="%{BKY_MATH_HUE}">
+              <Block type="math_number">
+                <Field name="NUM">123</Field>
+              </Block>
+              <Block type="math_arithmetic"></Block>
+              <Block type="math_single"></Block>
+              <Block type="math_trig"></Block>
+              <Block type="math_constant"></Block>
+              <Block type="math_number_property"></Block>
+              <Block type="math_round"></Block>
+              <Block type="math_on_list"></Block>
+              <Block type="math_modulo"></Block>
+              <Block type="math_constrain">
+                <Value name="LOW">
+                  <Block type="math_number">
+                    <Field name="NUM">1</Field>
+                  </Block>
+                </Value>
+                <Value name="HIGH">
+                  <Block type="math_number">
+                    <Field name="NUM">100</Field>
+                  </Block>
+                </Value>
+              </Block>
+              <Block type="math_random_int">
+                <Value name="FROM">
+                  <Block type="math_number">
+                    <Field name="NUM">1</Field>
+                  </Block>
+                </Value>
+                <Value name="TO">
+                  <Block type="math_number">
+                    <Field name="NUM">100</Field>
+                  </Block>
+                </Value>
+              </Block>
+              <Block type="math_random_float"></Block>
+              <Block type="math_atan2"></Block>
+            </Category>
+            <Category name="Lists" colour="%{BKY_LISTS_HUE}">
+              <Block type="lists_create_empty"></Block>
+              <Block type="lists_create_with"></Block>
+              <Block type="lists_repeat">
+                <Value name="NUM">
+                  <Block type="math_number">
+                    <Field name="NUM">5</Field>
+                  </Block>
+                </Value>
+              </Block>
+              <Block type="lists_length"></Block>
+              <Block type="lists_isEmpty"></Block>
+              <Block type="lists_indexOf"></Block>
+              <Block type="lists_getIndex"></Block>
+              <Block type="lists_setIndex"></Block>
+            </Category>
+            
+            <Category name="Variables" custom="VARIABLE" colour="%{BKY_VARIABLES_HUE}">
+            </Category>
+            <Category name="Functions" custom="PROCEDURE" colour="%{BKY_PROCEDURES_HUE}">
+            </Category>
+      
           </BlocklyComponent>
         </header>
       </div>
